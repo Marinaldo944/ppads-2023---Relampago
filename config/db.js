2a9e1896ef837c0780fb5030dc09221e7f9220d4
Marinaldo944 <marinaldo944@gmail.com>
2023-05-19 02:53:11 -0300

    novo caminho atlasDB

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production") {
-    module.exports = {mongoURI: "mongodb+srv://secretaria:sischamada@sistema1.wyese7z.mongodb.net/?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://secretaria:sischamada@sistema1.wyese7z.mongodb.net/"}
 }else{
     module.exports = {mongoURI: "mongodb://127.0.0.1/sischamada"}
 }
